5fa5c83675ffbe32d69e801009e1bbbd3ea455a4
devUserContact <developer.user.contact@protonmail.com>
2022-07-03 11:39:30 -0700

    deploy test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,40 +3,38 @@ import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  let ENVIRONMENT: string = process.env.NODE_ENV;
-  let apiURL: string = "";
+	let ENVIRONMENT: string = process.env.NODE_ENV;
+	let apiURL: string = "";
 
-  const [postList, setPostList] = useState([]);
+	const [postList, setPostList] = useState([]);
 
-
-  const getPosts = async () => {
-    const res = await fetch(apiURL);
-    const data = await res.json();
-    setPostList(data);
-  };
-  useEffect(() => {
-    if (ENVIRONMENT = "dev") {
-      apiURL = "http://localhost:3001/api/posts";
-    console.log(apiURL)
-    } else {
-      apiURL = "https://node-backend-server-ny288.ondigitalocean.app/api/posts";
-    console.log(apiURL)
-    }
-    getPosts();
-  }, []);
-  return (
-    <div className={styles.container}>
-      <>
-        {postList.map((post: any, post_id: number) => {
-          return (
-            <div className="outline" key={post_id}>
-              <p key={post_id}>{post.post}</p>
-            </div>
-          );
-        })}
-      </>
-    </div>
-  );
+	const getPosts = async () => {
+		const res = await fetch(apiURL);
+		const data = await res.json();
+		setPostList(data);
+	};
+	useEffect(() => {
+		if (ENVIRONMENT = "dev") {
+			apiURL = "http://localhost:3001/api/posts";
+		} else {
+			apiURL =
+				"https://node-backend-server-ny288.ondigitalocean.app/api/posts";
+		}
+		getPosts();
+	}, []);
+	return (
+		<div className={styles.container}>
+			<>
+				{postList.map((post: any, post_id: number) => {
+					return (
+						<div className="outline" key={post_id}>
+							<p key={post_id}>{post.post}</p>
+						</div>
+					);
+				})}
+			</>
+		</div>
+	);
 };
 
 export default Home;
