c00da609139e411d5c2e6556d1d5a5f0d9d7ff87
devUserContact <developer.user.contact@protonmail.com>
2022-07-13 13:21:35 -0700

    linting

--- pages/BlogTitle/index.tsx
+++ pages/BlogTitle/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import styles from "../../styles/Main.module.scss";
 export default function BlogTitle() {
-  return (
-  <>
-  <div className={styles.blogTitleContainer}> 
- 		<p className={styles.blogTitle}>devUserContact_blog	🐦</p>
- 		<p className={styles.blogSubTitle}>
- 			sharing my software development journey 
- 		</p>
-	</div>
-	</>
-  );
-}; 
+	return (
+		<>
+			<div className={styles.blogTitleContainer}>
+				<p className={styles.blogTitle}>devUserContact_blog 🐦</p>
+				<p className={styles.blogSubTitle}>
+					sharing my software development journey
+				</p>
+			</div>
+		</>
+	);
+}
