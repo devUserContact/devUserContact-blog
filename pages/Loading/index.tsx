d2b95043d0b49b5d4c54dbcaa347e9de5f6665eb
devUserContact <developer.user.contact@protonmail.com>
2022-07-18 21:47:05 -0700

    added loading component

---
+++ pages/Loading/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import styles from "../../styles/Main.module.scss";
+export default function BlogTitle() {
+	return (
+		<>
+			<div className={styles.blogTitleContainer}>
+				<p className={styles.postBody}>
+					Loading. . .
+				</p>
+			</div>
+		</>
+	);
+}
