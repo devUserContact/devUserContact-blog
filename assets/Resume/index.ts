9d834bed30a7fbb71a40277dd14c61ee8ed388f5
devUserContact <developer.user.contact@protonmail.com>
2022-08-03 21:36:50 -0700

    abstracted resume to assets directory

---
+++ assets/Resume/index.ts
@@ -0,0 +1,19 @@
+export const Resume: text = `### _Hello_, 
+### My name is Maxwell & I am a software developer with a passion for full-stack web development
+### 
+___
+projects: 
+- [Tabs to Markdown][1] - Extension for 🦊 Firefox
+- [Language Search Tool][2] - Next.js app for searching multiple language sites
+___
+contact:
+- [Email][email]
+- [GitHub][3]
+- [LinkedIn][4]
+
+[1]:https://addons.mozilla.org/en-US/firefox/addon/tabs-to-markdown/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search
+[2]:https://language-search-tool.netlify.app/
+[email]:mailto:developer.user.contact@protonamil.com "developer.user.contact@protonmail" 
+[3]:https://github.com/devUserContact
+[4]:https://www.linkedin.com/in/maxwell-wine-a23872221/?original_referer=https%3A%2F%2Fgithub.com%2FdevUserContact
+`;
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,27 +1,10 @@
 import type { NextPage } from "next";
 import Markdown from "react-markdown";
 import NavBar from "../../components/NavBar";
+import { Resume } from "../../assets/Resume"
 import styles from "../../styles/Main.module.scss";
 
-const resume = `### _Hello_, 
-### My name is Maxwell & I am a software developer with a passion for full-stack web development
-### 
-___
-projects: 
-- [Tabs to Markdown][1] - Extension for 🦊 Firefox
-- [Language Search Tool][2] - Next.js app for searching multiple language sites
-___
-contact:
-- [Email][email]
-- [GitHub · devUserContact][3]
-- [LinkedIn][4]
-
-[1]:https://addons.mozilla.org/en-US/firefox/addon/tabs-to-markdown/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search
-[2]:https://language-search-tool.netlify.app/
-[email]:mailto:developer.user.contact@protonamil.com "developer.user.contact@protonmail" 
-[3]:https://github.com/devUserContact
-[4]:https://www.linkedin.com/in/maxwell-wine-a23872221/?original_referer=https%3A%2F%2Fgithub.com%2FdevUserContact
-`;
+const resume = Resume; 
 
 const About: NextPage = () => {
 	return (
