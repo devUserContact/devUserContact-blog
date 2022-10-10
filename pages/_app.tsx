415901c6c6ab8228ea6e966b55fbc6bc53b6ddf5
devUserContact <developer.user.contact@protonmail.com>
2022-10-10 15:57:10 -0700

    implementing dev env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ import {
 	ApolloProvider,
 } from "@apollo/client";
 const client = new ApolloClient({
-	uri: "https://bluehappyface.net/api",
+	//uri: "https://bluehappyface.net/api",
+	uri: "http://localhost:8081/api",
 	cache: new InMemoryCache(),
 });
 
