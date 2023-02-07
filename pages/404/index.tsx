d896ce2dd0921f8c20f2c00364b77a76969962c6
devUserContact <developer.user.contact@protonmail.com>
2023-02-07 00:01:06 -0800

    added link to homepage on 404 page

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from 'react'
 import Image from 'next/image'
-import Message from "../../assets/devUserContact_404error.svg";
-import styles from "../../styles/Main.module.scss";
+import Message from '../../assets/devUserContact_404error.svg'
+import styles from '../../styles/Main.module.scss'
 
 const Message404 = () => {
-	const message = Message
-	return (
-		<>
-			<div className={styles.errorMessage}>
-				<Image src={message} alt={"404"} />
-			</div>
-			<div className={styles.errorMessage}>
-				<p className={styles.blogSubTitle}>page not found</p>
-			</div>
-		</>
-	)
-};
-export default Message404;
+  const message = Message
+  return (
+    <>
+      <div className={styles.errorMessage}>
+        <Image src={message} alt={'404'} />
+      </div>
+      <div className={styles.errorMessage}>
+        <p className={styles.blogSubTitle}>
+          page not found / <a href='https://devusercontact.com'>return home</a>
+        </p>
+      </div>
+    </>
+  )
+}
+export default Message404
