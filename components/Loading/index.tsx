695a506189b6e2c2a9650f27fd161fc99cd49d74
devUserContact <developer.user.contact@protonmail.com>
2023-03-07 22:17:08 -0800

    added useState hook to `loading` component

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from 'react';
 import styles from "../../styles/Main.module.scss";
 
 export default function Loading() {
+  const [loadingText, setLoadingText] = useState<string>("loading. . .");
 	return (
 		<>
 			<div className={styles.blogTitleContainer}>
 				<p className={styles.loadingText}>
-					Loading. . .
+          {loadingText}
 				</p>
 			</div>
 		</>
