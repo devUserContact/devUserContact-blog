5f9c9bc1c6a42dd48a044aa90e0c0dd33fac439d
devUserContact <developer.user.contact@protonmail.com>
2023-03-08 20:10:40 -0800

    implmented loading animation

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -2,7 +2,21 @@ import { useEffect, useState } from 'react';
 import styles from "../../styles/Main.module.scss";
 
 export default function Loading() {
-  const [loadingText, setLoadingText] = useState<string>("loading. . .");
+  const [toggle, setToggle] = useState<boolean>(true);
+  let loadingText = "loading. . ."
+    if (toggle === true) {
+      loadingText = "loading. . ." 
+    } else {
+      loadingText = "loading . . ."
+    }
+  function toggleBoolean() {
+    setTimeout(() => {
+      setToggle(!toggle);
+    }, 1000);
+  }
+  useEffect(() => {
+    toggleBoolean();
+  }, [toggleBoolean]);
 	return (
 		<>
 			<div className={styles.blogTitleContainer}>
