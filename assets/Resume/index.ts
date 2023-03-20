ebee58757623f11e3d619912597496b62cd7937a
devUserContact <developer.user.contact@protonmail.com>
2023-03-19 20:57:03 -0700

    updated resume

--- assets/Resume/index.ts
+++ assets/Resume/index.ts
@@ -2,6 +2,9 @@ export const Resume: any = `### _Hello_,
 ### My name is Maxwell & I am a software developer with a passion for full-stack web development
 ### 
 ___
+certificates
+- [Google Data Analytics Prefessional Certificate][googleDataCert] 
+___
 projects: 
 - [Emoji Fortune][1] - Next.js app that reveals your emoji fortune 🔮
 - [Language Search Tool][2] - Next.js app for searching multiple language sites 📚
@@ -18,4 +21,5 @@ contact:
 [4]:https://github.com/devUserContact
 [5]:https://www.linkedin.com/in/maxwell-wine-a23872221/?original_referer=https%3A%2F%2Fgithub.com%2FdevUserContact
 [email]:mailto:developer.user.contact@protonamil.com "developer.user.contact@protonmail" 
+[googleDataCert]:https://coursera.org/share/bb226f03aa86cc69c8f4bae652266074
 `;
