b552fa4c0fd8cf139292e4601862fcb811cbf17b
devUserContact <developer.user.contact@protonmail.com>
2025-07-18 18:17:57 -0700

    updated resume

--- src/assets/Resume/index.ts
+++ src/assets/Resume/index.ts
@@ -1,30 +1,30 @@
 export const Resume: any = `### _Hello_, 
-### My name is Maxwell & I am a software developer with a passion for full-stack web development
+### My name is Maxwell & I am a software developer with a passion for software development and cyber security.
 ### 
 ___
-certificates:
-- [TensorFlow Developer - '23][tensorFlowDeveloperCert] 
-- [Nvidia Fundamentals of Deep Learning - '23][nvidiaDeepLearingCert] 
-- [Google Data Analytics - '23][googleDataCert] 
+**Certificates:**
+- [CompTIA Security+][compTIA_security+] 
+- [TensorFlow Developer][tensorFlowDeveloperCert] 
+- [Nvidia Fundamentals of Deep Learning][nvidiaDeepLearningCert] 
+- [Google Data Analytics][googleDataCert] 
 ___
-projects: 
+**Projects:** 
 - [spdReader][appleAppStoreLink] - Read faster than ever before on iOS 📱
-- Tabs to Markdown - A Web Extension available for:
-  - [Chrome][ttmChrome] 🌎
-  - [Firefox][ttmFirefox] 🦊 
+- Tabs to Markdown - Convert all your open tabs to a Markdown file:
+  - [Chrome Web Store][ttmChrome] 🌎 (+100 users!)
+  - [Firefox addon][ttmFirefox] 🦊 
 ___
-contact:
-- [Email][email]
-- [GitHub][4]
-- [LinkedIn][5]
+**Contact:** 
+- [Email][email] | [GitHub][gitHub] | [LinkedIn][linkedIn]
 
-[ttmFirefox]:https://addons.mozilla.org/en-US/firefox/addon/tabs-to-markdown/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search
-[ttmChrome]:https://chrome.google.com/webstore/detail/tabs-to-markdown/hdejpgldbapinkbipleilelmhgcgffno
-[4]:https://github.com/devUserContact
-[5]:https://www.linkedin.com/in/maxwell-wine-a23872221/?original_referer=https%3A%2F%2Fgithub.com%2FdevUserContact
-[email]:mailto:developer.user.contact@protonamil.com "developer.user.contact@protonmail" 
-[googleDataCert]:https://coursera.org/share/bb226f03aa86cc69c8f4bae652266074
-[nvidiaDeepLearingCert]:https://courses.nvidia.com/certificates/2eb181e919b14add9c82353a6e6b3066/
+[compTIA_security+]:https://www.credly.com/badges/c6ece8cb-4e30-4fc7-8b2f-bf280fa0018a/public_url
 [tensorFlowDeveloperCert]:https://coursera.org/share/fd68740ab6367186d66ff0533a339411
+[nvidiaDeepLearningCert]:https://courses.nvidia.com/certificates/2eb181e919b14add9c82353a6e6b3066
+[googleDataCert]:https://coursera.org/share/bb226f03aa86cc69c8f4bae652266074
 [appleAppStoreLink]:https://apps.apple.com/us/app/spdreader/id6745416455
+[ttmChrome]:https://chrome.google.com/webstore/detail/tabs-to-markdown/hdejpgldbapinkbipleilelmhgcgffno
+[ttmFirefox]:https://addons.mozilla.org/en-US/firefox/addon/tabs-to-markdown/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search
+[email]:mailto:developer.user.contact@protonamil.com "developer.user.contact@protonmail" 
+[gitHub]:https://github.com/devUserContact
+[linkedIn]:https://www.linkedin.com/in/maxwell-wine-a23872221/?original_referer=https%3A%2F%2Fgithub.com%2FdevUserContact
 `;
